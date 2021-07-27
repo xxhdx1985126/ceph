3c13121b3c1fb10d573bef9c126d5b2d9199eb0c
Xuehan Xu <xxhdx1985126@gmail.com>
2021-07-27 23:00:56 +0800

    crimson/os/seastore: make fresh extents go though the extent placement manager
    Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

--- src/crimson/os/seastore/cached_extent.h
+++ src/crimson/os/seastore/cached_extent.h
@@ -23,6 +23,7 @@ class CachedExtent;
 using CachedExtentRef = boost::intrusive_ptr<CachedExtent>;
 class SegmentedAllocator;
 class TransactionManager;
+class ExtentPlacementManager;
 
 // #define DEBUG_CACHED_EXTENT_REF
 #ifdef DEBUG_CACHED_EXTENT_REF
@@ -476,6 +477,7 @@ protected:
   friend class crimson::os::seastore::OolExtent;
   friend class crimson::os::seastore::SegmentedAllocator;
   friend class crimson::os::seastore::TransactionManager;
+  friend class crimson::os::seastore::ExtentPlacementManager;
 };
 
 std::ostream &operator<<(std::ostream &, CachedExtent::extent_state_t);
@@ -570,7 +572,8 @@ public:
 
   void erase(CachedExtent &extent) {
     assert(extent.parent_index);
-    auto erased = extent_index.erase(extent);
+    auto erased = extent_index.erase(
+      extent_index.s_iterator_to(extent));
     extent.parent_index = nullptr;
 
     if (erased) {
