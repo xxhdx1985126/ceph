99738cc60fe022d1a138e760afdf7c807e4fc562
Xuehan Xu <xxhdx1985126@gmail.com>
2021-12-01 18:32:34 +0800

    crimson/os/seastore/segment_cleaner: add space reclaim related metrics
    Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

--- src/crimson/os/seastore/segment_cleaner.cc
+++ src/crimson/os/seastore/segment_cleaner.cc
@@ -189,6 +189,12 @@ void SegmentCleaner::register_metrics()
 		     sm::description("total number of extents released by SegmentCleaner")),
     sm::make_counter("accumulated_blocked_ios", stats.accumulative_blocked_ios,
 		     sm::description("accumulative total number of ios that were blocked by gc")),
+    sm::make_counter("reclaimed_segments", stats.reclaimed_segments,
+		     sm::description("reclaimed segments")),
+    sm::make_counter("reclaim_rewrite_bytes", stats.reclaim_rewrite_bytes,
+		     sm::description("rewritten bytes due to reclaim")),
+    sm::make_derive("reclaiming_rewrite_bytes", stats.reclaiming_rewrite_bytes,
+		    sm::description("current reclaiming segment's rewritten bytes")),
     sm::make_derive("empty_segments", stats.empty_segments,
 		    sm::description("current empty segments")),
     sm::make_derive("ios_blocking", stats.ios_blocking,
@@ -400,6 +406,7 @@ SegmentCleaner::gc_reclaim_space_ret SegmentCleaner::gc_reclaim_space()
                   "SegmentCleaner::gc_reclaim_space: addr {} alive, gc'ing {}",
                   addr,
                   *ext);
+		stats.reclaiming_rewrite_bytes += ext->get_length();
                 return ecb->rewrite_extent(
                   t,
                   ext);
@@ -416,6 +423,9 @@ SegmentCleaner::gc_reclaim_space_ret SegmentCleaner::gc_reclaim_space()
     });
   }).safe_then([this] {
     if (scan_cursor->is_complete()) {
+      stats.reclaim_rewrite_bytes += stats.reclaiming_rewrite_bytes;
+      stats.reclaiming_rewrite_bytes = 0;
+      stats.reclaimed_segments++;
       scan_cursor.reset();
     }
   });
