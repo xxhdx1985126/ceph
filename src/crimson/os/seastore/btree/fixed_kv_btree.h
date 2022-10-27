cdedcfd6050f33f95befeef949b446fc3fbfbbec
Xuehan Xu <xxhdx1985126@gmail.com>
2022-10-27 17:41:26 +0800

    crimson/os/seastore/transaction_manager: follow leaf<->logical extent pointers to read extent
    Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

--- src/crimson/os/seastore/btree/fixed_kv_btree.h
+++ src/crimson/os/seastore/btree/fixed_kv_btree.h
@@ -223,6 +223,9 @@ public:
       return leaf.node;
     }
 
+    uint16_t get_leaf_pos() {
+      return leaf.pos;
+    }
   private:
     iterator() noexcept {}
     iterator(depth_t depth) noexcept : internal(depth - 1) {}
@@ -1318,7 +1321,7 @@ private:
     };
 
     auto child_pos = parent->get_child(c.trans, node_iter);
-    auto &child = child_pos.child;
+    auto child = child_pos.template get_child<internal_node_t>();
     if (child) {
       SUBTRACET(seastore_fixedkv_tree,
         "got child on {}, pos: {}, res: {}",
@@ -1351,8 +1354,8 @@ private:
       begin,
       end,
       std::make_optional<node_position_t<internal_node_t>>(
-        child_pos.stable_parent->template cast<internal_node_t>(),
-        child_pos.pos)
+        child_pos.template get_parent<internal_node_t>(),
+        child_pos.get_pos())
     ).si_then([on_found=std::move(on_found)](InternalNodeRef node) {
       return on_found(node);
     });
@@ -1388,7 +1391,7 @@ private:
     };
 
     auto child_pos = parent->get_child(c.trans, node_iter);
-    auto &child = child_pos.child;
+    auto child = child_pos.template get_child<leaf_node_t>();
     if (child) {
       SUBTRACET(seastore_fixedkv_tree,
         "got child on {}, pos: {}, res: {}",
@@ -1421,8 +1424,8 @@ private:
       begin,
       end,
       std::make_optional<node_position_t<leaf_node_t>>(
-        child_pos.stable_parent->template cast<leaf_node_t>(),
-        child_pos.pos)
+        child_pos.template get_parent<leaf_node_t>(),
+        child_pos.get_pos())
     ).si_then([on_found=std::move(on_found)](LeafNodeRef node) {
       return on_found(node);
     });
@@ -1941,7 +1944,7 @@ private:
     };
 
     auto child_pos = parent_pos.node->get_child(c.trans, donor_iter);
-    auto &child = child_pos.child;
+    auto child = child_pos.template get_child<NodeType>();
     if (child) {
       SUBTRACET(seastore_fixedkv_tree,
         "got child on {}, pos: {}, res: {}",
@@ -1973,8 +1976,8 @@ private:
       begin,
       end,
       std::make_optional<node_position_t<NodeType>>(
-        child_pos.stable_parent->template cast<NodeType>(),
-        child_pos.pos)
+        child_pos.template get_parent<NodeType>(),
+        child_pos.get_pos())
     ).si_then([do_merge=std::move(do_merge)](typename NodeType::Ref donor) {
       return do_merge(donor);
     });
