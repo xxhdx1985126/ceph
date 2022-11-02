79b47a7d71dd761274cb16d4c6cc96b249acdb6d
Xuehan Xu <xxhdx1985126@gmail.com>
2022-11-02 16:59:09 +0800

    crimson/os/seastore/btree: drop btree_pin_set_t
    Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

--- src/crimson/os/seastore/btree/fixed_kv_btree.h
+++ src/crimson/os/seastore/btree/fixed_kv_btree.h
@@ -36,7 +36,6 @@ template <typename node_key_t>
 struct op_context_t {
   Cache &cache;
   Transaction &trans;
-  btree_pin_set_t<node_key_t> *pins = nullptr;
 };
 
 template <typename T>
@@ -352,7 +351,7 @@ public:
     root_leaf->set_size(0);
     fixed_kv_node_meta_t<node_key_t> meta{min_max_t<node_key_t>::min, min_max_t<node_key_t>::max, 1};
     root_leaf->set_meta(meta);
-    root_leaf->pin.set_range(meta);
+    root_leaf->range = meta;
     get_tree_stats<self_type>(c.trans).depth = 1u;
     get_tree_stats<self_type>(c.trans).extents_num_delta++;
     return {phy_tree_root_t{root_leaf->get_paddr(), 1u}, root_leaf};
@@ -867,7 +866,7 @@ public:
         fixed_kv_extent.get_length(),
         n_fixed_kv_extent->get_bptr().c_str());
       n_fixed_kv_extent->set_modify_time(fixed_kv_extent.get_modify_time());
-      n_fixed_kv_extent->pin.set_range(n_fixed_kv_extent->get_node_meta());
+      n_fixed_kv_extent->range = n_fixed_kv_extent->get_node_meta();
 
       if (fixed_kv_extent.get_type() == internal_node_t::TYPE ||
           leaf_node_t::do_has_children) {
@@ -1076,8 +1075,8 @@ private:
                           parent_pos=std::move(parent_pos)]
                           (internal_node_t &node) {
       assert(!node.is_pending());
-      assert(!node.pin.is_linked());
-      node.pin.set_range(fixed_kv_node_meta_t<node_key_t>{begin, end, depth});
+      assert(!node.is_linked());
+      node.range = fixed_kv_node_meta_t<node_key_t>{begin, end, depth};
       if (parent_pos) {
         auto &parent = parent_pos->node;
         parent->link_child(&node, parent_pos->pos);
@@ -1091,9 +1090,6 @@ private:
           assert(!root_block->is_pending());
           set_phy_tree_root_node<true, self_type>(root_block, &node);
         }
-      }
-      if (c.pins) {
-        c.pins->add_pin(node.pin);
       }
     };
     return c.cache.template get_extent<need_check_trans, internal_node_t>(
@@ -1111,7 +1107,7 @@ private:
         *ret);
       // This can only happen during init_cached_extent
       // or when backref extent being rewritten by gc space reclaiming
-      if (c.pins && !ret->is_pending() && !ret->pin.is_linked()) {
+      if (!ret->is_pending() && !ret->is_linked()) {
         assert(ret->is_dirty()
           || (is_backref_node(ret->get_type())
             && ret->is_clean()));
@@ -1154,8 +1150,8 @@ private:
                       parent_pos=std::move(parent_pos)]
                       (leaf_node_t &node) {
       assert(!node.is_pending());
-      assert(!node.pin.is_linked());
-      node.pin.set_range(fixed_kv_node_meta_t<node_key_t>{begin, end, 1});
+      assert(!node.is_linked());
+      node.range = fixed_kv_node_meta_t<node_key_t>{begin, end, 1};
       if (parent_pos) {
         auto &parent = parent_pos->node;
         parent->link_child(&node, parent_pos->pos);
@@ -1169,9 +1165,6 @@ private:
           assert(!root_block->is_pending());
           set_phy_tree_root_node<true, self_type>(root_block, &node);
         }
-      }
-      if (c.pins) {
-        c.pins->add_pin(node.pin);
       }
     };
     return c.cache.template get_extent<need_check_trans, leaf_node_t>(
@@ -1189,7 +1182,7 @@ private:
         *ret);
       // This can only happen during init_cached_extent
       // or when backref extent being rewritten by gc space reclaiming
-      if (c.pins && !ret->is_pending() && !ret->pin.is_linked()) {
+      if (!ret->is_pending() && !ret->is_linked()) {
         assert(ret->is_dirty()
           || (is_backref_node(ret->get_type())
             && ret->is_clean()));
@@ -1634,7 +1627,7 @@ private:
       fixed_kv_node_meta_t<node_key_t> meta{
         min_max_t<node_key_t>::min, min_max_t<node_key_t>::max, iter.get_depth() + 1};
       nroot->set_meta(meta);
-      nroot->pin.set_range(meta);
+      nroot->range = meta;
       nroot->journal_insert(
         nroot->begin(),
         min_max_t<node_key_t>::min,
