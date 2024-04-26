3c505ac7c355b3dcb1053c4b85020e5584e0f198
Xuehan Xu <xxhdx1985126@gmail.com>
2024-04-26 16:41:38 +0800

    test/osd/RadosModel: fix incorrect io results iterations
    Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

--- src/test/osd/RadosModel.h
+++ src/test/osd/RadosModel.h
@@ -1063,7 +1063,7 @@ public:
                     << " is already newer than " << (*i)->get_version64() << std::endl;
         }
 	(*i)->release();
-	waiting.erase(i++);
+	i = waiting.erase(i);
       }
 
       context->update_object_version(oid, version);
