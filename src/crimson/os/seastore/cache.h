45a23ee8934be781feda9272c35936dd0ce6042a
Xuehan Xu <xuxuehan@qianxin.com>
2025-06-29 17:41:44 +0800

    fixup! crimson/os/seastore/lba_manager: replace refresh_lba_cursor() with cursor.refresh()

--- src/crimson/os/seastore/cache.h
+++ src/crimson/os/seastore/cache.h
@@ -1794,7 +1794,7 @@ private:
     uint64_t hit = 0;
   };
 
-  cursor_stats_t cursor_stats;
+  btree_cursor_stats_t cursor_stats;
   struct invalid_trans_efforts_t {
     io_stat_t read;
     io_stat_t mutate;
--- src/crimson/os/seastore/transaction.h
+++ src/crimson/os/seastore/transaction.h
@@ -81,13 +81,13 @@ struct rewrite_stats_t {
   }
 };
 
-struct cursor_stats_t {
+struct btree_cursor_stats_t {
   uint64_t num_refresh_parent_total = 0;
   uint64_t num_refresh_invalid_parent = 0;
   uint64_t num_refresh_unviewable_parent = 0;
   uint64_t num_refresh_modified_viewable_parent = 0;
 
-  void apply(cursor_stats_t &stats) {
+  void apply(btree_cursor_stats_t &stats) {
     num_refresh_parent_total +=
       stats.num_refresh_parent_total;
     num_refresh_invalid_parent +=
@@ -618,7 +618,7 @@ public:
     return cache_hint;
   }
 
-  cursor_stats_t cursor_stats;
+  btree_cursor_stats_t cursor_stats;
 private:
   friend class Cache;
   friend Ref make_test_transaction();
